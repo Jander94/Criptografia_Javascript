84b5077fc9d0f5ec5f8ae1eeb5031f79111af8d3
Jander Gustavo <jander.gustavo@gmail.com>
2023-12-02 20:46:55 -0300

    Ajustando criptografia

--- script.js
+++ script.js
@@ -35,106 +35,25 @@ const alfabeto = [
   "x",
   "y",
   "z",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
 ];
 rl.question("1 - Criptografar \n2 - Descriptografar\n", (opt) => {
   opcao = opt;
   if (opcao == 1) {
     rl.question("Digite o texto: ", (txt) => {
       texto = txt;
-      rl.question(
-        "Digite um número de 0 a 20 para criptografar a mensagem: ",
-        (num) => {
-          numero = num;
-          criptografar();
-          rl.close();
-        }
-      );
+      rl.question("Digite uma chave para criptografar a mensagem: ", (text) => {
+        numero = gerarNum(text);
+        criptografar();
+        rl.close();
+      });
     });
   } else if (opcao == 2) {
     rl.question("Digite o texto: ", (txt) => {
       texto = txt;
       rl.question(
-        "Digite o número para descriptografar a mensagem: ",
-        (num) => {
-          numero = num;
+        "Digite a chave para descriptografar a mensagem: ",
+        (text) => {
+          numero = gerarNum(text);
           descriptografar();
           rl.close();
         }
@@ -154,7 +73,8 @@ const criptografar = () => {
       arrayCritp.push(l);
     } else {
       let posicao = alfabeto.indexOf(l);
-      arrayCritp.push(alfabeto[parseInt(posicao) + parseInt(numero)]);
+      let novaPosicao = (parseInt(posicao) + parseInt(numero)) % 26;
+      arrayCritp.push(alfabeto[novaPosicao]);
     }
   });
   textoCriptografado = arrayCritp.join("");
@@ -169,9 +89,18 @@ const descriptografar = () => {
       arrayCritp.push(l);
     } else {
       let posicao = alfabeto.indexOf(l);
-      arrayCritp.push(alfabeto[parseInt(posicao) - parseInt(numero)]);
+      let novaPosicao = (parseInt(posicao) - parseInt(numero) + 26) % 26;
+      arrayCritp.push(alfabeto[novaPosicao]);
     }
   });
   textoCriptografado = arrayCritp.join("");
   console.log("Texto descriptografado:\n\n", textoCriptografado, "\n\n");
+};
+
+const gerarNum = (text) => {
+  let n = 0;
+  text?.split("").forEach((t) => {
+    if (alfabeto.indexOf(t) > n) n = alfabeto.indexOf(t);
+  });
+  return n;
 };
